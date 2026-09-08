887f11a58b1160323b96716f10b7041905255064
Archit Patle <architpatle06@gmail.com>
2026-09-08 13:51:28 +0530

    Added ContactHero Section in Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ContactHero from '../components/Contact/ContactHero/ContactHero'
 
 const Contact = () => {
   return (
-    <div>
-      contact
-    </div>
+    <>
+      <ContactHero />
+    </>
   )
 }
 
